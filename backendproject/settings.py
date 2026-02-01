10515a482e2a6c89beee4cbc877a279da6b2a191
jeevitha344 <jeevithasagana3131@gmail.com>
2026-02-01 23:38:06 +0530

    fix

--- backendproject/settings.py
+++ backendproject/settings.py
@@ -92,25 +92,13 @@ WSGI_APPLICATION = 'backendproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import os
-import dj_database_url
-
-if os.getenv("ENV") == "PRODUCTION":
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=os.getenv("DATABASE_URL"),
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    # Local development uses SQLite
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASES={
+"default": dj_database_url.config(
+default=os.getenv("DATABASE_URL"),
+conn_max_age=600,
+ssl_require=True # Render PostgreSQL requires SSL
+)
+}
 
 
 # Password validation
