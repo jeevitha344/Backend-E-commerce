bcf8aec027e6238770af60351f06cf4128c0c145
jeevitha344 <jeevithasagana3131@gmail.com>
2026-02-01 23:09:14 +0530

    fix

--- backendapp/serializer.py
+++ backendapp/serializer.py
@@ -4,19 +4,20 @@ from django.contrib.auth.models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class productserializer(serializers.ModelSerializer):
+    
     product_category = serializers.PrimaryKeyRelatedField(
     queryset=product_categorytb.objects.all()
 )
-    product_image = serializers.SerializerMethodField()  # <-- add this
+    # product_image = serializers.SerializerMethodField()  # <-- add this
     class Meta:
         model = product_details
         fields = "__all__"
-        
-    def get_product_image(self, obj):
-        if obj.product_image:
-            return obj.product_image.url  # <-- this returns the Cloudinary URL
-        return ""
 
+    # def get_product_image(self, obj):
+    #     if obj.product_image:
+    #         return obj.product_image.url  # <-- this returns the Cloudinary URL
+    #     return ""
+   
 class ProductCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = product_categorytb
